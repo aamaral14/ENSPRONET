85d845c4407bd5dc9ccaf8d888dfff7378a43ba6
aamaral14 <alexandreamaral14@gmail.com>
2022-02-06 03:46:56 +0000

    created methods to insert countrys on db

--- ENSPRONET.Services/Services/Country/CountryService.cs
+++ ENSPRONET.Services/Services/Country/CountryService.cs
@@ -1,14 +1,48 @@
+using ENSPRONET.Services.Context;
+using Microsoft.EntityFrameworkCore;
+
 namespace ENSPRONET.Services.Services.Country;
 
-public class CountryService : ICountryReadService
+public class CountryService : ICountryReadService, ICountryCreateService
 {
-    public Task<List<Domains.Domains.Country>> List()
+    private readonly ENSPRONETContext ENSPRONETContext;
+    public CountryService(ENSPRONETContext ensproNetContext)
     {
-        throw new NotImplementedException();
+        ENSPRONETContext = ensproNetContext;
     }
 
-    public Task<Domains.Domains.Country> ReadByAlpha2Code(string code)
+    public async Task<int> Create(Domains.Domains.Country country)
     {
-        throw new NotImplementedException();
+        if (country == null)
+            throw new ArgumentNullException("country");
+
+        ENSPRONETContext.Add(country);
+
+        await ENSPRONETContext.SaveChangesAsync();
+
+        return country.Id;
+    }
+
+    public async Task CreateBulk(List<Domains.Domains.Country> countries)
+    {
+        if (countries == null || countries?.Count == 0)
+            throw new ArgumentNullException("countries");
+
+        ENSPRONETContext.AddRange(countries);
+
+        await ENSPRONETContext.SaveChangesAsync();
+    }
+
+    public async Task<List<Domains.Domains.Country>> List()
+    {
+        return ENSPRONETContext.Countries.ToList();
+    }
+
+    public async Task<Domains.Domains.Country> ReadByAlpha2Code(string code)
+    {
+        if (String.IsNullOrEmpty(code))
+            throw new ArgumentNullException(code);
+
+        return await ENSPRONETContext.Countries.FirstOrDefaultAsync(m => m.Alpha2Code == code);
     }
 }
